ce82ef1643f6b6effec214307e5b41da6748f3e3
Amiel Asparen <amielasparen@gmail.com>
2015-05-27 23:31:09 +0800

    profile image added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526171716) do
+ActiveRecord::Schema.define(version: 20150527150658) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
